3be3bba19d41b33ef6d1a25a0ff1301ec0f7befe
Rahul-Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-08-12 18:22:44 +0530

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,5 @@
+#Write a python script to print values of three variables, each in a new line. Variable contains integer values
+var_1=10
+var_2=20
+var_3=999
+print(var_1,var_2,var_3,sep='\n')
---
+++ p9.py
@@ -0,0 +1,6 @@
+#Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name='Rahul Bhutaiya'
+age=20
+qualification='BCA T.Y.'
+experience=0
+print(name,age,qualification,experience)
